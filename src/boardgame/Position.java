da5bd975485803ca510015a1a7fcb87a755df554
jxxErick <ericksdantas144@gmail.com>
2021-10-10 15:55:42 -0300

    chess layer

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,5 +1,33 @@
 package boardgame;
 
 public class Position {
+	private int row;
+	private int column;
+	
+	public Position(int row, int collumn) {
+		this.row = row;
+		this.column = collumn;
 
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getCollumn() {
+		return column;
+	}
+
+	public void setCollumn(int collumn) {
+		this.column = collumn;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
 }
